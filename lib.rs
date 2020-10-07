2ce6311f6db53c44f6bc3964d09116cac0bd1652
DESKTOP\Vadim <support@zavodil.ru>
2020-10-07 03:35:04 +0300

    NEAR Pools details contract added

--- lib.rs
+++ lib.rs
@@ -4,6 +4,10 @@ use near_sdk::wee_alloc;
 use near_sdk::{env, ext_contract, near_bindgen};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use near_sdk::Gas;
+
+const BASE: Gas = 25_000_000_000_000;
+pub const CALLBACK: Gas = BASE * 2;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
@@ -28,6 +32,7 @@ pub trait ExtPoolDetails {
     fn on_get_owner_id(
         &mut self,
         #[callback] get_owner_id: String,
+        staking_pool_account_id: String,
         pool_id: String,
         name: String,
         value: String,
@@ -43,19 +48,33 @@ pub struct PoolDetails {
 #[near_bindgen]
 impl PoolDetails {
     pub fn update_field(&mut self, pool_id: String, name: String, value: String) -> bool {
+        assert!(
+            pool_id != "",
+            "Abort. Pool ID is empty"
+        );
+
+        assert!(
+            name != "",
+            "Abort. Name is empty"
+        );
+
+        assert!(
+            value != "",
+            "Abort. Value is empty"
+        );
+
         let _owner_account_id_to_compare: String = env::signer_account_id().clone();
 
-        env::log(format!("_owner_account_id_to_compare {}", _owner_account_id_to_compare).as_bytes());
-
         staking_pool::get_owner_id(
-            &pool_id, 0, 175000000000000)
+            &pool_id, 0, BASE)
             .then(ext_self_owner::on_get_owner_id(
                 _owner_account_id_to_compare,
                 pool_id,
                 name,
-                &value,
+                value,
+                &env::current_account_id(),
                 0,
-                175000000000000,
+                CALLBACK
             ));
 
         true
@@ -65,6 +84,24 @@ impl PoolDetails {
         &self.fields
     }
 
+    fn get_field_id(&self, pool_id: String, name: String) -> u64 {
+        // TODO please help to optimize loop
+        for (_key, value) in &self.fields {
+            if value.pool_id == pool_id && value.name == name {
+                env::log(format!("Field {} updated for pool {}",name, pool_id).as_bytes());
+                return value.field_id;
+            }
+        }
+        env::log(format!("Field {} added for pool {}", name, pool_id).as_bytes());
+        return *self.fields.keys().max().unwrap_or(&0u64) + 1;
+    }
+
+    /*
+    pub fn get_all_field_by_pool(&self, pool_id :String) -> Option<Field> {
+        // TODO need to optimize get_field_id first
+    }
+    */
+
     pub fn on_get_owner_id(
         &mut self,
         #[callback] owner_id: String,
@@ -73,19 +110,16 @@ impl PoolDetails {
         name: String,
         value: String,
     ) -> bool {
-        env::log(format!("owner_id {}", owner_id).as_bytes());
+        assert_self();
 
         assert!(
             owner_id == staking_pool_account_id,
-            "You are not the owner of pool"
+            "You are not the owner of pool. Login as {} in order to update {}",
+            owner_id,
+            pool_id
         );
 
-        assert!(
-            name == "",
-            "Name is empty"
-        );
-
-        let field_id = *self.fields.keys().max().unwrap_or(&0u64) + 1;
+        let field_id = self.get_field_id(pool_id.clone(), name.clone());
 
         self.fields.insert(
             field_id,
@@ -98,5 +132,9 @@ impl PoolDetails {
         );
 
         true
+    }
+
+    pub fn assert_self() {
+        assert_eq!(env::predecessor_account_id(), env::current_account_id());
     }
 }
