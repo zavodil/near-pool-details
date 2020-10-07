30f5996ec6132e33ad90d4ae51a364479ce0863b
DESKTOP\Vadim <support@zavodil.ru>
2020-10-07 15:47:52 +0300

    Data structure refactored

--- lib.rs
+++ lib.rs
@@ -2,7 +2,6 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::wee_alloc;
 use near_sdk::{env, ext_contract, near_bindgen};
-use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use near_sdk::collections::UnorderedMap;
 use near_sdk::Gas;
@@ -14,19 +13,10 @@ pub const CALLBACK: Gas = BASE * 2;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[derive(Debug, Clone, Default, BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
-pub struct Field {
-    pub field_id: u64,
-    pub pool_id: String,
-    pub name: String,
-    pub value: String,
-}
-
-//type FieldsByPools = near_sdk::collections::UnorderedMap<u64, Vec<Field>>;
 type PoolId = String;
 type FieldName = String;
 type FieldValue = String;
-type FieldsStorageByPoolId = UnorderedMap<PoolId, UnorderedMap<FieldName, FieldValue>>;
+type FieldsStorageByPoolId = UnorderedMap<PoolId, HashMap<FieldName, FieldValue>>;
 
 #[ext_contract(staking_pool)]
 pub trait StakingPool {
@@ -86,30 +76,15 @@ impl PoolDetails {
         true
     }
 
-    pub fn get_all_fields(&self) -> HashMap<String, UnorderedMap<FieldName, FieldValue>> {
+
+    pub fn get_all_fields(&self) -> HashMap<PoolId, HashMap<FieldName, FieldValue>> {
         self.fields_by_pool.iter().collect()
     }
 
-    /*
-    fn get_field_id(&self, pool_id: String, name: String) -> u64 {
-        // TODO please help to optimize loop
-        for (_key, value) in &self.fields {
-            if value.pool_id == pool_id && value.name == name {
-                env::log(format!("Field {} updated for pool {}",name, pool_id).as_bytes());
-                return value.field_id;
-            }
-        }
-        env::log(format!("Field {} added for pool {}", name, pool_id).as_bytes());
 
-        return self.fields.keys().max().unwrap_or(0u64) + 1;
+    pub fn get_fields_by_pool(&self, pool_id: String) -> Option<HashMap<FieldName, FieldValue>> {
+        self.fields_by_pool.get(&pool_id)
     }
-*/
-
-    /*
-    pub fn get_all_field_by_pool(&self, pool_id :String) -> Option<Field> {
-        // TODO need to optimize get_field_id first
-    }
-    */
 
     pub fn on_get_owner_id(
         &mut self,
@@ -128,22 +103,14 @@ impl PoolDetails {
             pool_id
         );
 
-        let mut pool_fields = self.fields_by_pool.get(&pool_id).unwrap_or_default();
-        pool_fields.insert(&name, &value);
+        env::log(format!("Field {} added for pool {}", name, pool_id).as_bytes());
 
-        /*
-        let field_id = self.get_field_id(pool_id.clone(), name.clone());
+        let mut fields = self.fields_by_pool.get(&pool_id).unwrap_or_default();
+        fields.insert(name, value);
 
-        self.fields.insert(
-            &field_id,
-            &vec![Field {
-                field_id,
-                pool_id,
-                name,
-                value,
-            }],
-        );
-        */
+        self.fields_by_pool.insert(
+            &pool_id,
+            &fields);
 
         true
     }
